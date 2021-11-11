9e903fb867d4e996f447fb40421f53e47f4b83e8
tandy <tandy1000@cock.li>
2021-11-11 22:46:51 +0000

    remove forward declarations..

--- uniony.nim
+++ uniony.nim
@@ -3,11 +3,6 @@ import pkg/union
 import pkg/union/uniontraits
 
 
-proc parseHook*[T: Union](s: string, i: var int, v: var T)
-
-proc dumpHook*[T: Union](s: var string, v: T)
-
-
 proc parseHook*[T: Union](s: string, i: var int, v: var T) =
   unpack(v):
     var unpacked = it

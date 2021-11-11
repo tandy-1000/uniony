8fe8fab4844b836d28a8651f908733da3dc8028a
tandy <tandy1000@cock.li>
2021-11-11 22:47:24 +0000

    oops forgot to remove debug echos

--- uniony.nim
+++ uniony.nim
@@ -7,8 +7,6 @@ proc parseHook*[T: Union](s: string, i: var int, v: var T) =
   unpack(v):
     var unpacked = it
     parseHook(s, i, unpacked)
-    echo "unpacked", unpacked
-    echo "it", it
     v <- unpacked
 
 

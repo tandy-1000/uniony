253e50571ed4b964ff37aaac707e500cd244e526
tandy <tandy1000@cock.li>
2021-11-11 22:42:06 +0000

    fix dumpHook by @alaviss

--- uniony.nim
+++ uniony.nim
@@ -5,18 +5,18 @@ import pkg/union/uniontraits
 
 proc parseHook*[T: Union](s: string, i: var int, v: var T)
 
-proc dumpHook*[T: Union](s: var string, v: var T)
+proc dumpHook*[T: Union](s: var string, v: T)
 
 
 proc parseHook*[T: Union](s: string, i: var int, v: var T) =
   unpack(v):
     var unpacked = it
     parseHook(s, i, unpacked)
+    echo "unpacked", unpacked
+    echo "it", it
     v <- unpacked
 
 
-proc dumpHook*[T: Union](s: var string, v: var T) =
+proc dumpHook*[T: Union](s: var string, v: T) =
   unpack(v):
-    var unpacked = it
-    dumpHook(s, unpacked)
-    v <- unpacked
+    dumpHook(s, it)
